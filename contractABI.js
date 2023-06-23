35753085773b07ae3d1f1a75f7be68e59eda21bc
Amanpradhan <aman.pradhan@conversenow.ai>
2023-06-23 18:40:41 +0530

    fixed ethers library issue and added button to register user

---
+++ contractABI.js
@@ -0,0 +1,520 @@
+export const contractABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "bytes32",
+				"name": "id",
+				"type": "bytes32"
+			}
+		],
+		"name": "ChainlinkCancelled",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "bytes32",
+				"name": "id",
+				"type": "bytes32"
+			}
+		],
+		"name": "ChainlinkFulfilled",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "bytes32",
+				"name": "id",
+				"type": "bytes32"
+			}
+		],
+		"name": "ChainlinkRequested",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "rider",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "rideId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "rating",
+				"type": "uint256"
+			}
+		],
+		"name": "DriverRated",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "bytes32",
+				"name": "_requestId",
+				"type": "bytes32"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "_status",
+				"type": "string"
+			}
+		],
+		"name": "RequestVolume",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "driver",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "rideId",
+				"type": "uint256"
+			}
+		],
+		"name": "RideAccepted",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "rider",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "rideId",
+				"type": "uint256"
+			}
+		],
+		"name": "RideCancelled",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "rider",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "rideId",
+				"type": "uint256"
+			}
+		],
+		"name": "RideCompleted",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "rider",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "uint256",
+				"name": "rideId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "fare",
+				"type": "uint256"
+			}
+		],
+		"name": "RideRequested",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "UserRegistered",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rideId",
+				"type": "uint256"
+			}
+		],
+		"name": "acceptRide",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rideId",
+				"type": "uint256"
+			}
+		],
+		"name": "cancelRide",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rideId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_rating",
+				"type": "uint256"
+			}
+		],
+		"name": "completeRideAndRateDriver",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "distance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "_requestId",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "string",
+				"name": "_status",
+				"type": "string"
+			}
+		],
+		"name": "fulfillDistance",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getPricePerMeter",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_rideId",
+				"type": "uint256"
+			}
+		],
+		"name": "getRide",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "address payable",
+						"name": "rider",
+						"type": "address"
+					},
+					{
+						"internalType": "address payable",
+						"name": "driver",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "fare",
+						"type": "uint256"
+					},
+					{
+						"internalType": "bool",
+						"name": "rideCompleted",
+						"type": "bool"
+					},
+					{
+						"internalType": "string",
+						"name": "estdTime",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "rating",
+						"type": "uint256"
+					}
+				],
+				"internalType": "struct RideHailing.Ride",
+				"name": "ride",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "pricePerMeter",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "registerUser",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "registeredUsers",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_pickup",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_drop",
+				"type": "string"
+			}
+		],
+		"name": "requestRide",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "rideCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "rides",
+		"outputs": [
+			{
+				"internalType": "address payable",
+				"name": "rider",
+				"type": "address"
+			},
+			{
+				"internalType": "address payable",
+				"name": "driver",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "fare",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bool",
+				"name": "rideCompleted",
+				"type": "bool"
+			},
+			{
+				"internalType": "string",
+				"name": "estdTime",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "rating",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "newPrice",
+				"type": "uint256"
+			}
+		],
+		"name": "setPricePerMeter",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "status",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	}
+];
--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-const ethers = require('ethers');
-const { Web3Provider } = require('ethers/providers');
+// import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
+import { ethers } from "./node_modules/ethers/dist/ethers.esm.min.js";
+import { contractABI } from './contractABI.js';
+
+// Use the contractABI in your code...
+
+// const { Web3Provider } = require('ethers/providers');
 // import contractABI from './contractABI.js';
+// console.log("ABI = ", contractABI);
+const requestButton = document.getElementById('requestButton');
+const registerUserButton = document.getElementById('registerUserButton');
+
+const pickupInput = document.getElementById('pickupInput');
+const dropInput = document.getElementById('dropInput');
+const connectButton = document.getElementById('connectButton');
+
+requestButton.addEventListener('click', () => {
+    const pickup = pickupInput.value;
+    const drop = dropInput.value;
+    requestRide(pickup, drop);
+});
+
+registerUserButton.addEventListener('click', () => {
+    registeredUser();
+});
+
+connectButton.addEventListener('click', connectToMetaMask);
 
 // Connect to MetaMask provider
 const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -9,62 +33,72 @@ const signer = provider.getSigner();
 // Load the RideHailing contract using its address and ABI
 const contractAddress = '0x3B148D7b70A940015D9626D0878B5B5B6Fdbf8f5';
 
-async function loadContractABI() {
-  try {
-    const response = await fetch('./contractABI.json');
-    const json = await response.json();
-    const contractABI = JSON.parse(json);
-    
-    // Use the contractABI in your code...
-  } catch (error) {
-    console.error(error);
-  }
-}
+// async function loadContractABI() {
+//   try {
+//     const response = await fetch('./contractABI.json');
+//     const json = await response.json();
+//     const contractABI = JSON.parse(json);
 
-loadContractABI();
+//     // Use the contractABI in your code...
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// loadContractABI();
 
 const contract = new ethers.Contract(contractAddress, contractABI, signer);
 
 // Request MetaMask to connect
 async function connectToMetaMask() {
-  try {
-    // Request user's permission to connect
-    console.log("A");
-    await window.ethereum.enable();
-    // Refresh the page after connecting
-    window.location.reload();
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        // Request user's permission to connect
+        console.log("A");
+        const metamask = await window.ethereum.enable();
+        console.log("connected", metamask)
+        // Refresh the page after connecting
+        // window.location.reload();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 // Example function for interacting with the contract
 async function requestRide(pickup, drop) {
-  try {
-    // Call the contract function
-    const tx = await contract.requestRide(pickup, drop, { value: ethers.utils.parseEther('0.1') });
-    await tx.wait();
-    console.log('Ride requested');
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        // Call the contract function
+        const tx = await contract.requestRide(pickup, drop, { value: ethers.utils.parseEther('0.1') });
+        await tx.wait();
+        console.log('Ride requested');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function registeredUser() {
+    try {
+        const registerUser = await contract.registerUser();
+        console.log("Registered User = ", registerUser);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 // Example function for getting the price per meter from the contract
 async function getPricePerMeter() {
-  try {
-    const pricePerMeter = await contract.getPricePerMeter();
-    console.log('Price per meter:', pricePerMeter.toString());
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const pricePerMeter = await contract.getPricePerMeter();
+        console.log('Price per meter:', pricePerMeter.toString());
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 // Check if MetaMask is connected
 if (window.ethereum) {
-  // MetaMask is available
-  connectToMetaMask(); // Automatically connect on page load
+    // MetaMask is available
+    connectToMetaMask(); // Automatically connect on page load
 } else {
-  // MetaMask is not available
-  console.error('Please install MetaMask extension to use this application.');
+    // MetaMask is not available
+    console.error('Please install MetaMask extension to use this application.');
 }
